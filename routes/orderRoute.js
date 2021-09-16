62f0a9be2bc22002471b8002c98cde18cd2d99c2
thurpe <babatope.ifesanya@gmail.com>
2021-09-16 23:51:45 +0100

    Added correction and styling

--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -1,101 +1,82 @@
-const express = require('express');
-const {v4: uuidv4} = require('uuid');
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 const router = express.Router();
-const stripe = require('stripe')("sk_test_51JYvLNCOUZhrfRQLF9pmsSNEmEiSudWqcCd48exZT4YhD0zUJmuvU34gsrT7iM9RpQQIZGV3gtqjWXeEKPvOkwft00uggkqLcL")
-const Order = require('../models/orderModel')
+const stripe = require("stripe")(
+  "sk_test_51JYvLNCOUZhrfRQLF9pmsSNEmEiSudWqcCd48exZT4YhD0zUJmuvU34gsrT7iM9RpQQIZGV3gtqjWXeEKPvOkwft00uggkqLcL"
+);
+const Order = require("../models/orderModel");
 
-router.post("/placeorder", async(req, res) => {
+router.post("/placeorder", async (req, res) => {
+  const { token, cartItems, currentUser, subtotal } = req.body;
 
-    const {token , cartItems , currentUser , subtotal} = req.body
+  const customer = await stripe.customers.create({
+    email: token.email,
+    source: token.id,
+  });
 
-    const customer = await stripe.customers.create({
-        email : token.email , 
-        source : token.id
-    })
-
-    const payment = await stripe.charges.create({
-          amount : subtotal*100 , 
-          currency : 'inr' , 
-          customer : customer.id , 
-          receipt_email : token.email
-    } , {
-        idempotencyKey: uuidv4()
-    })
-
-
-    if(payment)
+  const payment = await stripe.charges.create(
     {
-        const order = new Order({
-
-              userid : currentUser._id ,
-              name : currentUser.name,
-              email : currentUser.email ,
-              orderItems : cartItems ,
-              shippingAddress : {
-                  address : token.card.address_line1 ,
-                  city : token.card.address_city,
-                  country : token.card.address_country,
-                  postalCode : token.card.addres_zip
-              },
-              orderAmount : subtotal,
-              transactionId : payment.source.id ,
-              isDelivered : false
-
-
-        })
-
-        order.save(err=>{
-
-            if(err)
-            {
-                return res.status(400).json({ message: 'Something went wrong' });
-            }
-            else{
-                res.send('Order Placed Successfully')
-            }
-
-        })
+      amount: subtotal * 100,
+      currency: "inr",
+      customer: customer.id,
+      receipt_email: token.email,
+    },
+    {
+      idempotencyKey: uuidv4(),
     }
-    else{
-        return res.status(400).json({ message: 'Payment failed' });
-    }
-  
+  );
+
+  if (payment) {
+    const order = new Order({
+      userid: currentUser._id,
+      name: currentUser.name,
+      email: currentUser.email,
+      orderItems: cartItems,
+      shippingAddress: {
+        address: token.card.address_line1,
+        city: token.card.address_city,
+        country: token.card.address_country,
+        postalCode: token.card.address_zip,
+      },
+      orderAmount: subtotal,
+      transactionId: payment.source.id,
+      isDelivered: false,
+    });
+
+    order.save((err) => {
+      if (err) {
+        return res.status(400).json({ message: "Something went wrong" });
+      } else {
+        res.send("Order Placed Successfully");
+      }
+    });
+  } else {
+    return res.status(400).json({ message: "Payment failed" });
+  }
 });
-
 
 router.post("/getordersbyuserid", (req, res) => {
+  const userid = req.body.userid;
 
-    const userid = req.body.userid
-
-    Order.find({userid : userid} , (err , docs)=>{
-
-        if(err){
-            return res.status(400).json({ message: 'something went wrong' });
-        }
-        else{
-            res.send(docs)
-        }
-
-    })
-  
+  Order.find({ userid: userid }, (err, docs) => {
+    if (err) {
+      return res.status(400).json({ message: "something went wrong" });
+    } else {
+      res.send(docs);
+    }
+  });
 });
-
 
 router.post("/getorderbyid", (req, res) => {
+  const orderid = req.body.orderid;
 
-    const orderid = req.body.orderid
-
-    Order.find({_id: orderid} , (err , docs)=>{
-
-        if(err){
-            return res.status(400).json({ message: 'something went wrong' });
-        }
-        else{
-            res.send(docs[0])
-        }
-
-    })
-  
+  Order.find({ _id: orderid }, (err, docs) => {
+    if (err) {
+      return res.status(400).json({ message: "something went wrong" });
+    } else {
+      res.send(docs[0]);
+    }
+  });
 });
 
-module.exports = router
+module.exports = router;
